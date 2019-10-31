e2a5427cf0b81dd30ae5a053896f33b9e96df9f8
Rajitha <rajithaeye@gmail.com>
2019-10-31 13:58:18 +0530

    intercepts in components

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,21 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+
+export default {
+  name: 'about',
+  components: {
+  },
+  
+  beforeRouteLeave (to:any, from:any, next:any) {
+  const answer = window.confirm('Do you really want to leave? you have unsaved changes!')
+  if (answer) {
+    next()
+  } else {
+    next(false)
+  }
+}
+}
+</script>
