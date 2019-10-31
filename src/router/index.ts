d39bc29b03af8d4b91ce0b34fa9038d9ae1e9b33
Rajitha <rajithaeye@gmail.com>
2019-10-31 13:28:27 +0530

    lazy loaded routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue';
+// import Home from '../views/Home.vue'
+// import About from '../views/About.vue';
 
 Vue.use(VueRouter)
 
@@ -9,12 +9,12 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/about',
     name: 'about',
-    component: About
+    component: () => import('../views/About.vue')
   }
 ]
 
